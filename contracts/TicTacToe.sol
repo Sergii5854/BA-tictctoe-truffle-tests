e8ba7b357e1267adae9e3e703d6feeddaf7df252
Sergii Bondarenko <bondarenko.sergii.i@gmail.com>
2019-01-23 17:02:04 +0200

    assert.equal all methodth

--- contracts/TicTacToe.sol
+++ contracts/TicTacToe.sol
@@ -3,124 +3,137 @@ pragma solidity 0.5.0;
 
 contract TicTacToe
 {
-   uint8 stepCounter ;
-   bool public  isReady;
-   address public activeUser;
-   address public player1; // 1 = o
-   address public player2; // 2 = x
-   uint8[][] public board;
-   int8 public winner = -1;
+    uint8 stepCounter;
+    bool public  isReady;
+    address public activeUser;
+    address public player1; // 1 = o
+    address public player2; // 2 = x
+    uint8[][] public board;
+    uint8 public gameWinner = 0;
 
-   constructor() public {
-         isReady = true;
-         stepCounter = 0;
-         initArray();
-   }
+    constructor() public {
+        isReady = true;
+        stepCounter = 0;
+        initArray();
+    }
 
-   modifier onlyActiveUser() {
-       require( msg.sender == activeUser );
-       _;
-   }
+    modifier onlyActiveUser() {
+        require(msg.sender == activeUser);
+        _;
+    }
 
-   function initArray() internal  {
-       board = new uint8[][](3);
-       for (uint8 i=0; i < 3; i++) {
-           uint8[] memory temp = new uint8[](3);
-           for(uint8 j = 0; j < 3; j++){
-                   temp[j]=0;
-               }
-           board[i] = temp;
-       }
-   }
+    function initArray() internal {
+        board = new uint8[][](3);
+        for (uint8 i = 0; i < 3; i++) {
+            uint8[] memory temp = new uint8[](3);
+            for (uint8 j = 0; j < 3; j++) {
+                temp[j] = 0;
+            }
+            board[i] = temp;
+        }
+    }
 
-   function setUser() public{
+    function setUser() public {
 
-       require(
-           player1 == address(0) ||
-           player2 == address(0)
-       );
+        require(
+            player1 == address(0) ||
+            player2 == address(0)
+        );
 
-       if(player1 == address(0) ){
-           player1 = msg.sender;
-       }else if(player2 == address(0)){
-           player2 = msg.sender;
-           activeUser = player2;
-           isReady = false;
-           for(uint8 i = 0; i < 2; i++ ){
-               for(uint8 j = 0; j < 2; j++ ){
-                   board[i][j] = 0;
-               }
-           }
-       }
+        if (player1 == address(0)) {
+            player1 = msg.sender;
+        } else if (player2 == address(0)) {
+            player2 = msg.sender;
+            activeUser = player2;
+            isReady = false;
+            uint8 counter = 3;
+            for (uint8 i = 0; i < 2; i++) {
+                for (uint8 j = 0; j < 2; j++) {
+                    board[i][j] = counter;
+                    counter++;
+                }
+            }
+        }
 
-   }
-   function drow(uint8 col, uint8 row) onlyActiveUser  public {
-       require( row < 3  && col < 3 );
-       require( board[col][row] == 0 );
-       stepCounter = stepCounter + 1;
+    }
 
-       if(msg.sender == player1 ){
-           board[col][row] = 1;
-           activeUser = player2;
+    function drow(uint8 col, uint8 row) onlyActiveUser public {
+        require(row < 3 && col < 3);
+        require( board[col][row] != 1 && board[col][row] != 2 );
 
-       }else if(msg.sender == player2) {
+        stepCounter = stepCounter + 1;
+
+        if (msg.sender == player1) {
+            board[col][row] = 1;
+            activeUser = player2;
+
+        } else if (msg.sender == player2) {
             board[col][row] = 2;
             activeUser = player1;
-       }
+        }
 
-       if(!checkBoard() ){
-           checkWinner();
-           reset();
-       }
-   }
+        checkWinner();
 
-   function reset() internal{
-       player1 = address(0);
-       player2 = address(0);
-       isReady = true;
-       stepCounter = 0;
+        if (!checkBoard()) {
+//            checkWinner();
+            reset();
+        }
+    }
 
-   }
-   function checkWinner() internal{
+    function reset() internal {
+        player1 = address(0);
+        player2 = address(0);
+        isReady = true;
+        stepCounter = 0;
 
-   //[(0,0),(0,1),(0,2)]
-   //[(1,0),(1,1),(1,2)]
-   //[(2,0),(2,1),(2,2)]
-           if(
-               board[1][1] ==  board[0][1]  &&  board[1][1] ==  board[2][1] ||
-               board[1][1] ==  board[0][0]  &&  board[1][1] ==  board[2][2] ||
-               board[1][1] ==  board[0][2]  &&  board[1][1] ==  board[2][0] ||
-               board[1][1] ==  board[1][0]  &&  board[1][1] ==  board[1][2] )
-               {
-               winner =  int8(board[1][1]);
-            }else if(
-                board[0][1] ==  board[0][0]  &&  board[0][1] ==  board[0][2] ||
-                board[1][0] ==  board[0][0]  &&  board[1][0] ==  board[2][0]
-                ){
-                 winner =  int8(board[0][0]);
-            }else if(
-               board[2][1] ==  board[2][0]  &&  board[2][1] ==  board[2][2] ||
-               board[1][2] ==  board[0][2]  &&  board[1][2] ==  board[2][2]){
-                    winner =  int8(board[2][2]);
-            }else{
-                winner = 0;
+    }
+
+    function checkWinner() internal {
+
+        //[(0,0),(0,1),(0,2)]
+        //[(1,0),(1,1),(1,2)]
+        //[(2,0),(2,1),(2,2)]
+
+
+        if (
+            board[1][1] == board[0][1] && board[1][1] == board[2][1] || // center-v
+            board[1][1] == board[0][0] && board[1][1] == board[2][2] || // back-slash
+            board[1][1] == board[0][2] && board[1][1] == board[2][0] || // slash
+            board[1][1] == board[1][0] && board[1][1] == board[1][2])   // center-h
+        {
+            gameWinner = board[1][1];
+        } else if (
+            board[0][1] == board[0][0] && board[0][1] == board[0][2] || //
+            board[1][0] == board[0][0] && board[1][0] == board[2][0]
+        ) {
+            gameWinner = board[0][0];
+        } else if (
+            board[2][1] == board[2][0] && board[2][1] == board[2][2] ||
+            board[1][2] == board[0][2] && board[1][2] == board[2][2]) {
+            gameWinner = board[2][2];
+        } else {
+            gameWinner = 0;
+        }
+
+
+    }
+
+    function checkBoard() internal view returns (bool){
+
+        for (uint8 i = 0; i < 2; i++) {
+            for (uint8 j = 0; j < 2; j++) {
+                if (board[i][j] == 0) {
+                    return true;
+                }
             }
+        }
 
+        return false;
+    }
 
+    function getCell(uint8 col, uint8 row) public view returns (uint8){
 
-   }
-
-   function checkBoard() internal view returns(bool){
-
-       for(uint8 i = 0; i < 2; i++ ){
-           for(uint8 j = 0; j < 2; j++ ){
-              if( board[i][j] == 0) {
-                  return true;
-              }
-           }
-       }
-
-       return false;
-   }
+        return board[col][row];
+    }
 
 }
--- test/ticTacToeEthTest.js
+++ test/ticTacToeEthTest.js
@@ -1,16 +1,45 @@
+const TicTacToe = artifacts.require("./TicTacToe");
 
 
-const TicTacToe = artifacts.require("TicTacToe");
-contract("TicTacToe", accounts => {
-    it("should put 54 coin in the first account", () =>
-        TicTacToe.deployed()
-            .then(instance => instance.getBalance.call(accounts[0]))
-            .then(balance => {
-                assert.equal(
-                    balance.valueOf(),
-                    54000,
-                    "10000 wasn't in the first account"
-                );
-            }));
+contract(" TicTacToe test", async accounts => {
+    var instance;
+    var player1 = accounts[0]
+    var player2 = accounts[1]
+
+    beforeEach(" instance ", async () => {
+        instance = await TicTacToe.new();
+
+        instance.setUser({from: player1 })
+        instance.setUser({from: player2 })
+        assert.equal( await instance.player1(), player1);
+        assert.equal( await instance.player2(), player2);
+        assert.equal( await instance.activeUser(), player2);
+        // await instance.drow(0,0 , {from:player2})
+        console.log('getCell',(await instance.getCell(0, 0)).toNumber());
+
+
+        instance.drow( 0,0 , {from:player2 })
+        instance.drow( 1,0 , {from:player1 })
+        instance.drow( 0,1 , {from:player2 })
+        instance.drow( 1,1 , {from:player1 })
+        instance.drow( 0,2 , {from:player2 })
+        console.log('getCell 0 0 ___ ',(await instance.getCell(0, 0)).toNumber());
+        console.log('getCell 0 1 ___ ',(await instance.getCell(0, 1)).toNumber());
+        console.log('getCell 0 2 ___',(await instance.getCell(0, 2)).toNumber());
+        console.log( "1111111111", ( await instance.gameWinner() ).toNumber() ) ;
+
+    });
+    //
+    // it("check init variables ", async () => {
+    //     assert.equal( await instance.isReady(), true);
+    //     // assert.equal( await instance.stepCounter.call(), 0);
+    //     assert.equal( await instance.activeUser(), '0x0000000000000000000000000000000000000000');
+    //     assert.equal( await instance.player1(),  '0x0000000000000000000000000000000000000000');
+    //     assert.equal( await instance.player2(),  '0x0000000000000000000000000000000000000000');
+    // });
+
+    it("check scenario  TicTacToe Game ", async () => {
+        // assert.equal( await instance.player2(),  '0x0000000000000000000000000000000000000000');
+    });
 
 })
